9c718632ba8c3472fe2ee3fe6957f355418b10ac
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-10 15:39:28 +0300

    commit tree fc5492afd0bd8d6a3e32e83fece6d7d51910bfb0

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+/*const totalBatteries = batteryBatches.reduce=>(function(sum, total){
+  return total + sum
+  }
+*/
+
+/*const totalBatteries = batteryBatches.reduce(function(sum, total){
+    return total + sum
+})*/
+
+const totalBatteries = batteryBatches.reduce((sum, total) => total + sum
+)
